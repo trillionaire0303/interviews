2a01ca2dd2492ac4e9a9b2b0cdabe66c521ea382
trillionaire0303 <trillionaire0303@gmail.com>
2022-05-25 17:56:55 +0900

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,17 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8GTFLKEPT0"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-8GTFLKEPT0');
+        </script>
+
+        
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>면접 리스트</title>
       </Head>
