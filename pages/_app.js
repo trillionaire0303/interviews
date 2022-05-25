d11a305727f4a51006116b1025f8236b5df6bb00
trillionaire0303 <trillionaire0303@gmail.com>
2022-05-25 18:06:06 +0900

    make it better

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,25 @@
+import React, { useEffect } from 'react';
 import Head from "next/head";
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [ router.events ])
+  
   return (
     <>
       <Head>
-
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8GTFLKEPT0"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'G-8GTFLKEPT0');
-        </script>
-
-        
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>면접 리스트</title>
       </Head>
